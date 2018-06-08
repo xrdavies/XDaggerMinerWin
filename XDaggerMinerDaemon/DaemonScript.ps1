f38f7518f211e8a6f41c2e851e7cc4f471ca0726
DESKTOP-2QR5VUO\charl <charlie_5899@hotmail.com>
2018-06-08 15:43:00 -0700

    Refine the Daemon Script.

--- XDaggerMinerDaemon/DaemonScript.ps1
+++ XDaggerMinerDaemon/DaemonScript.ps1
@@ -7,7 +7,7 @@ Param(
 
 Process {
 
-	Function CleanupMinerService {
+	Function UninstallMinerService {
 
 		Write-Host 'Checking Miner Service.'
 		
@@ -17,37 +17,72 @@ Process {
 			Write-Host 'Miner Service Found, Stopping the service...'
 	
 		    $service | Stop-Service -Force
-		    Start-sleep -s 10
+		    Start-Sleep -s 3
 			
 			Write-Host 'Miner Service Stopped.'
 			
 			# Uninstall the service
 			Write-Host 'Deleting the service...'
-			Start-Process -FilePath 'C:\Windows\Microsoft.NET/Framework64\v4.0.30319\InstallUtil.exe' -ArgumentList '/u D:\Temp\xdagger\XDaggerMinerWin\XDaggerMinerService.exe' -NoNewWindow
+			Start-Process -FilePath 'C:\Windows\Microsoft.NET/Framework64\v4.0.30319\InstallUtil.exe' -ArgumentList '/u .\XDaggerMinerService.exe' -NoNewWindow
 			Write-Host 'Miner Service Deleted.'	
-		}	
+		}
+		else
+		{
+			Write-Host 'Cannot find Miner Service.'
+		}
+	}
+
+	Function StartMinerService {
+
+		Write-Host 'Checking Miner Service.'
+		
+		$service = Get-WmiObject -Class Win32_Service -Filter "Name = 'XDaggerMinerService'"
+		if ($service -ne $null) 
+		{
+			Write-Host 'Miner Service Found, Starting the service...'
+	
+		    $service | Start-Service
+		    Start-Sleep -s 3
+			
+			Write-Host 'Miner Service Started.'
+		}
+		else
+		{
+			Write-Host 'Cannot find Miner Service.'
+		}
+	}
+	
+	Function StopMinerService {
+
+		Write-Host 'Checking Miner Service.'
+		
+		$service = Get-WmiObject -Class Win32_Service -Filter "Name = 'XDaggerMinerService'"
+		if ($service -ne $null) 
+		{
+			Write-Host 'Miner Service Found, Stopping the service...'
+	
+		    $service | Stop-Service -Force
+		    Start-Sleep -s 3
+			
+			Write-Host 'Miner Service Stopped.'
+		}
+		else
+		{
+			Write-Host 'Cannot find Miner Service.'
+		}
 	}
 
 	Function InstallMinerService {
 		
 		Write-Host 'Installing the Miner Service.'
-		Start-Process -FilePath 'C:\Windows\Microsoft.NET/Framework64\v4.0.30319\InstallUtil.exe' -ArgumentList 'D:\Temp\xdagger\XDaggerMinerWin\XDaggerMinerService.exe' -NoNewWindow
+
+		# Clean up first to make sure it does not exist
+		UninstallMinerService
+
+		Start-Process -FilePath 'C:\Windows\Microsoft.NET/Framework64\v4.0.30319\InstallUtil.exe' -ArgumentList '.\XDaggerMinerService.exe' -NoNewWindow
 		Start-Sleep -s 3
 		
 		Write-Host 'Miner Service Installed.'
-		
-		$service = Get-WmiObject -Class Win32_Service -Filter "Name = 'XDaggerMinerService'"
-		if ($service -ne $null) 
-		{
-			Write-Host 'Starting Miner Service...'
-			$service | Start-Service
-		    Write-Host 'Miner Service Started.'
-			
-		}
-		else
-		{
-			Write-Host 'Did not find Miner Service.'
-		}
 	}
 	
 	if ($Operation.Equals("InstallService", [System.StringComparison]::InvariantCultureIgnoreCase))
@@ -56,6 +91,22 @@ Process {
 		return '{ "result":"0"}'
 	}
 
-	
+	if ($Operation.Equals("UninstallService", [System.StringComparison]::InvariantCultureIgnoreCase))
+	{
+		InstallMinerService
+		return '{ "result":"0"}'
+	}
+
+	if ($Operation.Equals("StartService", [System.StringComparison]::InvariantCultureIgnoreCase))
+	{
+		StartMinerService
+		return '{ "result":"0"}'
+	}
+
+	if ($Operation.Equals("StopService", [System.StringComparison]::InvariantCultureIgnoreCase))
+	{
+		StopMinerService
+		return '{ "result":"0"}'
+	}
 }
 
